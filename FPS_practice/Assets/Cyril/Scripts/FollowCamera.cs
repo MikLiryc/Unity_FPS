1ffed4d2a23b1232bf557c421ddf4283295b1c18
DESKTOP-GLD38TH\KGA_11 <jay.kim1994@gmail.com>
2020-11-24 15:07:11 +0900

    Added Press Space to Jump Press 1 to change to First Person, press 3 to change to Thrid Person added Lerp move of camera during changing point of view

---
+++ FPS_practice/Assets/Cyril/Scripts/FollowCamera.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;                        //카메라가 따라다닐 타겟
+    public Transform targetFirst;                   //1인칭 시점
+    public Transform focusPoint;
+    public float speed = 10.0f;                     //카메라 이동속도
+
+    public bool isFPS = true;
+    public bool isOnSetting = false;
+
+    private Vector3 FPSPos;
+    private Vector3 TPSPos;
+    private Vector3 FPSFocus;
+    private Vector3 TPSFocus; 
+
+    void Start()
+    {
+        FPSPos = new Vector3(0f, 1f, 0f);
+        TPSPos = new Vector3(0f, 4f, -4f);
+        FPSFocus = new Vector3(0f, 0.8f, 5f);
+        TPSFocus = new Vector3(0f, 0.8f, 0f);
+    }
+
+    void Update()
+    {
+        //FollowTarget();
+
+        //FPS to TPS, TPS to FPS
+        ChangeView();
+        LookAtTarget();
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        if (isFPS)
+        {
+            //카메라의 위치를 강제로 타겟에 고정
+            if (transform.localPosition != FPSPos)
+            {
+                isOnSetting = true;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, FPSPos, 3 * Time.deltaTime);
+                focusPoint.localPosition = Vector3.Lerp(focusPoint.localPosition, FPSFocus, 3 * Time.deltaTime);
+                transform.LookAt(focusPoint);
+                float distance = (FPSPos - transform.localPosition).sqrMagnitude;
+                if (distance < 0.01f)
+                {
+                    transform.localPosition = FPSPos;
+                    focusPoint.localPosition = FPSFocus;
+                    isOnSetting = false;
+                }
+            }
+        }
+        else
+        {
+            if (transform.localPosition != TPSPos)
+            {
+                isOnSetting = true;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, TPSPos, 2 * Time.deltaTime);
+                focusPoint.localPosition = Vector3.Lerp(focusPoint.localPosition, TPSFocus, 2 * Time.deltaTime);
+                float distance = (TPSPos - transform.localPosition).sqrMagnitude;
+                if (distance < 0.01f)
+                {
+                    transform.localPosition = TPSPos;
+                    focusPoint.localPosition = TPSFocus;
+                    isOnSetting = false;
+                }
+            }
+        }
+    }
+
+    private void LookAtTarget()
+    {
+        if (!isFPS || isOnSetting)
+        {
+            transform.LookAt(focusPoint);
+        }
+    }
+
+    private void ChangeView()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            isFPS = true;
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            isFPS = false;
+        }
+    }
+
+    private void FollowTarget()
+    {
+        //타겟 방향 구하기
+        //방향 = 타겟 - 자신
+        Vector3 dir = target.position - transform.position;
+        dir.Normalize();
+        
+        transform.Translate(dir * speed * Time.deltaTime);
+
+        //일정거리 이상 붙으면 고정시키는 방법
+        //1. Distance() 사용
+        //if (Vector3.Distance(target.position, transform.position) < 1.0f) transform.position = target.position;
+        
+        //2. magnitude 사용
+        //float distance = (target.position - transform.position).magnitude; //dir.magnitude랑 똑같은듯?
+        //if (distance < 1.0f) transform.position = target.position;
+        
+        //3. sqrMagnitude 사용 (정확한 값은 아니지만, 제곱근 연산을 하지 않기 때문에, 연산량이 적어 크기만 비교할 때 많이 사용한다)
+        float distance1 = (target.position - transform.position).sqrMagnitude;
+        if (distance1 < 1.0f) transform.position = target.position;
+
+        //위와 같이 했는데도 카메라가 흔들리는 경우
+        //FollowTarget 을 LateUpdate()에 넣으면 해결됨...
+
+    }
+}
--- FPS_practice/Assets/Cyril/Scripts/PlayerController.cs
+++ FPS_practice/Assets/Cyril/Scripts/PlayerController.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+delegate void CamRotationDel();
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float walkSpeed = 5.0f;
     [SerializeField]
-    private float jumpForce = 3.0f;
+    private float jumpForce = 8.0f;
+    private float gravity = 15.0f;
 
     private float currentSpeed;
-    
+
     private float mx = 0f;
     private float my = 0f;
 
@@ -25,12 +28,22 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private Camera playerCamera;
-    private Rigidbody playerRigidbody;
+    private CharacterController cCon;
+    private CamRotationDel camDel;
+    private FollowCamera fCam;
+    private bool isGrounded = false;
+
+    private Vector3 moveDir;
 
     private void Start()
     {
+        camDel = new CamRotationDel(CameraVerticalRotation);
+        camDel += CameraHorizontalRotation;
 
-        playerRigidbody = GetComponent<Rigidbody>();
+        moveDir = Vector3.zero;
+
+        fCam = playerCamera.GetComponent<FollowCamera>();
+        cCon = GetComponent<CharacterController>();
         currentSpeed = walkSpeed;
     }
 
@@ -38,9 +51,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CharacterMove();
-        CameraRotation();
+        
+        camDel();
+        //CameraRotation();
         //CameraVerticalRotation();
         //CameraHorizontalRotation();
+
+        TryJump();
+        CheckGrounded();
     }
 
     private void CharacterMove()
@@ -51,26 +69,28 @@ public class PlayerController : MonoBehaviour
         //플레이어가 바라보고 있는 방향에 상관없이 world space 기준 상하좌우 방향 계산
         //Vector3 dir = new Vector3(dirX, .0f, dirZ).normalized;
         //playerRigidbody.MovePosition(transform.position + dir * currentSpeed * Time.deltaTime);
-
-
-        //플레이어가 바라보고 있는 방향 기준 상하좌우 방향 계싼
+        
+        //플레이어가 바라보고 있는 방향 기준 상하좌우 방향 계산
         Vector3 moveHor = transform.right * dirX;
         Vector3 moveVer = transform.forward * dirZ;
-        
+
         Vector3 velocity = (moveHor + moveVer).normalized * currentSpeed;
-        
-        playerRigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
+
+        cCon.Move(velocity * Time.deltaTime);
     }
 
     //화면 상하 회전
     private void CameraVerticalRotation()
     {
-        float xRotation = Input.GetAxisRaw("Mouse Y");
-        //float xRotation = Input.GetAxisRaw("Vertical");
-        float cameraRotationX = -xRotation * lookSensitivity;
-        currentCameraRotaitonX += cameraRotationX;
-        currentCameraRotaitonX = Mathf.Clamp(currentCameraRotaitonX, -cameraRoationLimit, cameraRoationLimit);
-        playerCamera.transform.localEulerAngles = new Vector3(currentCameraRotaitonX, 0f, 0f);
+        if (fCam.isFPS && !fCam.isOnSetting)
+        {
+            float xRotation = Input.GetAxisRaw("Mouse Y");
+            //float xRotation = Input.GetAxisRaw("Vertical");
+            float cameraRotationX = -xRotation * lookSensitivity;
+            currentCameraRotaitonX += cameraRotationX;
+            currentCameraRotaitonX = Mathf.Clamp(currentCameraRotaitonX, -cameraRoationLimit, cameraRoationLimit);
+            playerCamera.transform.localEulerAngles = new Vector3(currentCameraRotaitonX, 0f, 0f);
+        }
     }
 
     //화면 좌우 회전
@@ -79,7 +99,7 @@ public class PlayerController : MonoBehaviour
         float yRotation = Input.GetAxisRaw("Mouse X");
         //float yRotation = Input.GetAxisRaw("Horizontal");
         Vector3 characterRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
-        playerRigidbody.MoveRotation(playerRigidbody.rotation * Quaternion.Euler(characterRotationY));
+        transform.Rotate(characterRotationY);
     }
 
     //마우스를 따라 카메라가 회전 - 한번에 상하좌우 모두 처리
@@ -93,7 +113,44 @@ public class PlayerController : MonoBehaviour
         my += v * rotSpeed * Time.deltaTime;
 
         my = Mathf.Clamp(my, -cameraRoationLimit, cameraRoationLimit);
-        playerCamera.transform.eulerAngles = new Vector3(-my, mx, 0f);
+        playerCamera.transform.eulerAngles = new Vector3(-my, 0f, 0f);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, mx, transform.eulerAngles.z);
     }
+
+    private void TryJump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            Jump();
+        }
+        ApplyGravity();
+    }
+
+    private void Jump()
+    {
+        moveDir.y = jumpForce;
+    }
+
+    private void ApplyGravity()
+    {
+        moveDir.y -= gravity * Time.deltaTime;
+        cCon.Move(moveDir * Time.deltaTime);
+    }
+
+    private void CheckGrounded()
+    {
+        var playerHeight = cCon.height / 2;
+        var playerRadius = cCon.radius;
+        var maxDistance = playerHeight - playerRadius - 0.05f;
+        Ray sphRay = new Ray(new Vector3(0, cCon.center.y - playerHeight + playerRadius, 0f), transform.up * -1);
+        RaycastHit hit;
+
+        Physics.SphereCast(cCon.center, playerRadius, -transform.up, out hit, maxDistance);
+
+        isGrounded = cCon.isGrounded;
+        // Physics.SphereCast (레이저를 발사할 위치, 구의 반경, 발사 방향, 충돌 결과, 최대 거리)
+        
+        //isGrounded = cCon.isGrounded;
+        //Debug.Log(isGrounded);
+   }
 }
